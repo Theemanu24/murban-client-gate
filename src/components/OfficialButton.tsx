a95d0df58174ca84dc65fb9ba17e0d4974cb10c3
Emmanuel Baraka <emmbaraka23@gmail.com>
2025-08-31 14:47:04 +0300

    feat: redesign official site button

--- src/components/OfficialButton.tsx
+++ src/components/OfficialButton.tsx
@@ -1,24 +1,18 @@
 const OfficialButton = () => (
-  <a href="https://murban-eng.com" target="_blank" rel="noopener noreferrer">
-    <button className="official-button">
-      Official Website
-      <div className="icon-1">
-        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12 0L16 12H8z" />
-        </svg>
-      </div>
-      <div className="icon-2">
-        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <circle cx="12" cy="12" r="6" />
-        </svg>
-      </div>
-      <div className="icon-3">
-        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <rect x="4" y="4" width="16" height="16" />
-        </svg>
-      </div>
-    </button>
-  </a>
+  <div className="official-button-container">
+    <a
+      href="https://murban-eng.com"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="official-button official-button--type-c"
+    >
+      <div className="official-button__line" />
+      <div className="official-button__line" />
+      <span className="official-button__text">Official Website</span>
+      <div className="official-button__drow1" />
+      <div className="official-button__drow2" />
+    </a>
+  </div>
 );
 
 export default OfficialButton;
